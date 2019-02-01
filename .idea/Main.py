8060a7ad4376bbe109f8dce24ff4d18cef525dc5
amuroni <muronialessandro@gmail.com>
2019-02-01 16:04:35 +0100

    Initial commit

---
+++ .idea/Main.py
@@ -0,0 +1,16 @@
+import gspread
+import pprint as pp#this one is needed to format correctly the data, otherwise it is not legible
+from oauth2client.service_account import ServiceAccountCredentials
+
+# credentials to create a client to interact with the GDrive API
+scope = ["https://spreadsheets.google.com/feeds",
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name("FinancialModeling.json", scope)
+client = gspread.authorize(creds)
+
+# adding the workbook by name
+# double check the name is correct or it will return error
+
+sheet = client.open("BaseModel").sheet1  # returns weird values from sheet CP (control panel) with checks - TRY TO UNDERSTAND HOW TO USE THIS
+values = sheet.get_all_records()
+pp.pprint(values)
