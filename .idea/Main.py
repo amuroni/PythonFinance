94ea2d9f7dae76a61539ae7568f468c8565113e6
amuroni <muronialessandro@gmail.com>
2019-02-01 17:29:09 +0100

    fixes

--- .idea/Main.py
+++ .idea/Main.py
@@ -9,7 +9,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("FinancialModeling.json
 client = gspread.authorize(creds)
 
 # adding the workbook by name
-# double check the name is correct or it will return error
+# double check the name of the excel file is correct or it will return error
 
 sheet = client.open("BaseModel").sheet1  # returns weird values from sheet CP (control panel) with checks - TRY TO UNDERSTAND HOW TO USE THIS
 values = sheet.get_all_records()
