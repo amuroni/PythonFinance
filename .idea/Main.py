3696b04e7020e6af4f0e5645bde3a953f198b3b3
amuroni <muronialessandro@gmail.com>
2019-02-02 10:01:11 +0100

    base code update; used documentation variable for creds; can load a spreadsheet, select a worksheet and edit cells. the spreadsheet automatically updates itself.

--- .idea/Main.py
+++ .idea/Main.py
@@ -2,15 +2,27 @@ import gspread
 import pprint as pp#this one is needed to format correctly the data, otherwise it is not legible
 from oauth2client.service_account import ServiceAccountCredentials
 
-# credentials to create a client to interact with the GDrive API
+# add credentials - create client to interact with GDrive and GSheets API
+# renamed credentials and gc after the documentation for correct implementation of the library
+
 scope = ["https://spreadsheets.google.com/feeds",
          'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name("FinancialModeling.json", scope)
-client = gspread.authorize(creds)
+credentials = ServiceAccountCredentials.from_json_keyfile_name("FinancialModeling.json", scope)
+gc = gspread.authorize(credentials)
 
 # adding the workbook by name
 # double check the name of the excel file is correct or it will return error
 
-sheet = client.open("BaseModel").sheet1  # returns weird values from sheet CP (control panel) with checks - TRY TO UNDERSTAND HOW TO USE THIS
-values = sheet.get_all_records()
-pp.pprint(values)
+sh = gc.open("BaseModel")  # loading the excel file
+
+F1Y = sh.worksheet("F1Y")  # this one opens the actual worksheet in the spreadsheet
+# val = F1Y.acell("A20").value  # select a specific cell in the worksheet
+# print(val)  # printing the value just to check (should be "Ammortamenti") = OK!
+
+# now we have to try and edit a cell in the actual excel file, and se if it updates the value
+
+data = sh.worksheet("DATA")
+# pp.pprint(data.range("B3:D17"))  test printing a range in the data worksheet = OK!
+
+data.update_acell("D5", 100000)  # editing the value of a cell in sheet data
+pp.pprint(data.acell("D5"))  # print to check the value, should return 100k
