10f6399c5890054978120555af778d3774fb92c6
Alessandro Muroni <muronialessandro@gmail.com>
2019-06-28 14:35:26 +0200

    New version, working with a local excel file (which I will not commit for now)

--- .idea/v2/Main.py
+++ .idea/v2/Main.py
@@ -1,4 +1,8 @@
-import openpyxl as pyxl
-from openpyxl import *
+import xlwings as xw
 
-wb = load_workbook(filename = "Model.xlsx")  # load the excel file
+wb = xw.Book("Model.xlsx")  # load the excel file
+
+summary = wb.sheets["Summary"]  # select a sheet
+irr = summary.range("H22").value # select a cell, the IRR in this case
+print("{:.2%}".format(irr))  # limitation in openpyxl, switched to xlwings
+
